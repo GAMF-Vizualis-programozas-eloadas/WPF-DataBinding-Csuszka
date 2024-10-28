3bd260f724f2176375917b52e127987d32036261
jzscsaba <johanyak.csaba@nje.hu>
2024-10-28 16:55:43 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WPF_Adatkotes_Csuszka
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+	}
+
+}
